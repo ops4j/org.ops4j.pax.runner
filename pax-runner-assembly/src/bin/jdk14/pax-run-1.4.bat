d286ad61cbafb91158db9daa78b9d9308deb398c
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2007-10-09 16:21:09 +0000

    PAXRUNNER-57 Add quotes(") after the jar.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@8899 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner-assembly/src/bin/jdk14/pax-run-1.4.bat
+++ pax-runner-assembly/src/bin/jdk14/pax-run-1.4.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -jar "%_SCRIPTS_%\pax-runner-${project.version}-jdk14.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %JAVA_OPTS% -jar "%_SCRIPTS_%\pax-runner-${project.version}-jdk14.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
