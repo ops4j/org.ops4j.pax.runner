ac6210e510ed77309826a57335423603616c7428
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-02-23 04:54:25 +0000

    Next version will be 0.8.0.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@10669 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner/pax-run-1.4.sh
+++ pax-runner/pax-run-1.4.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.7.1-SNAPSHOT-jdk14.jar "$@"
+java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.8.0-SNAPSHOT-jdk14.jar "$@"
