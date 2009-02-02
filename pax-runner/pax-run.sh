1e26cc9cb3407ce3f417caa5c96c585e8782b9cd
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-02-02 11:24:32 +0000

    Prepare for 0.17.2
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@14080 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner/pax-run.sh
+++ pax-runner/pax-run.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.17.1-SNAPSHOT.jar "$@"
+java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.17.2-SNAPSHOT.jar "$@"
