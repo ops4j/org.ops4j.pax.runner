ef1856342c7259cb433a584751c0dd1efdc79a63
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-03-20 03:03:31 +0000

    PAXRUNNER-224 Implement scan-maven: (first step)
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@14577 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

---
+++ pax-runner-scanner-maven/src/main/java/org/ops4j/pax/runner/scanner/maven/internal/MavenScanner.java
@@ -0,0 +1,349 @@
+/*
+ * Copyright 2009 Alin Dreghiciu.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.runner.scanner.maven.internal;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.maven.mercury.artifact.Artifact;
+import org.apache.maven.mercury.artifact.ArtifactBasicMetadata;
+import org.apache.maven.mercury.artifact.ArtifactMetadata;
+import org.apache.maven.mercury.artifact.Quality;
+import org.apache.maven.mercury.artifact.QualityRange;
+import org.apache.maven.mercury.builder.api.DependencyProcessor;
+import org.apache.maven.mercury.repository.api.ArtifactBasicResults;
+import org.apache.maven.mercury.repository.api.ArtifactResults;
+import org.apache.maven.mercury.repository.api.Repository;
+import org.apache.maven.mercury.repository.api.RepositoryException;
+import org.apache.maven.mercury.repository.local.m2.LocalRepositoryM2;
+import org.apache.maven.mercury.repository.remote.m2.RemoteRepositoryM2;
+import org.apache.maven.mercury.repository.virtual.VirtualRepositoryReader;
+import org.apache.maven.mercury.transport.api.Server;
+import org.ops4j.lang.NullArgumentException;
+import org.ops4j.pax.runner.provision.BundleReference;
+import org.ops4j.pax.runner.provision.MalformedSpecificationException;
+import org.ops4j.pax.runner.provision.ProvisionSpec;
+import org.ops4j.pax.runner.provision.Scanner;
+import org.ops4j.pax.runner.provision.ScannerException;
+import org.ops4j.pax.runner.provision.scanner.FileBundleReference;
+import org.ops4j.pax.runner.provision.scanner.ScannerConfiguration;
+import org.ops4j.pax.runner.provision.scanner.ScannerConfigurationImpl;
+import org.ops4j.pax.runner.scanner.maven.ServiceConstants;
+import org.ops4j.pax.url.maven.commons.MavenConfiguration;
+import org.ops4j.pax.url.maven.commons.MavenConfigurationImpl;
+import org.ops4j.pax.url.maven.commons.MavenRepositoryURL;
+import org.ops4j.pax.url.maven.commons.MavenSettingsImpl;
+import org.ops4j.util.property.PropertyResolver;
+
+/**
+ * A scanner that scans maven artifacts for main artifact (if a jar/bundle), direct dependencies and transitive
+ * dependencies.
+ *
+ * @author Alin Dreghiciu
+ * @since 0.18.0, March 19, 2007
+ */
+public class MavenScanner
+    implements Scanner
+{
+
+    /**
+     * Logger.
+     */
+    private static final Log LOGGER = LogFactory.getLog( MavenScanner.class );
+    /**
+     * PropertyResolver used to resolve properties.
+     */
+    private PropertyResolver m_propertyResolver;
+    /**
+     * Scanner configuration.
+     */
+    private ScannerConfiguration m_scannerConfiguration;
+    /**
+     * Mercury virtual repository reader.
+     */
+    private VirtualRepositoryReader m_vrr;
+    /**
+     * Exception in case that thrown during initialization.
+     */
+    private Exception m_exception;
+
+    /**
+     * Creates a new file scanner.
+     *
+     * @param propertyResolver a propertyResolver; mandatory
+     */
+    public MavenScanner( final PropertyResolver propertyResolver )
+    {
+        NullArgumentException.validateNotNull( propertyResolver, "PropertyResolver" );
+        m_propertyResolver = propertyResolver;
+        try
+        {
+            initialize();
+            m_exception = null;
+        }
+        catch( Exception e )
+        {
+            m_exception = e;
+        }
+    }
+
+    /**
+     * Reads the bundles from the file specified by the urlSpec.
+     * {@inheritDoc}
+     */
+    public List<BundleReference> scan( final ProvisionSpec provisionSpec )
+        throws MalformedSpecificationException, ScannerException
+    {
+        NullArgumentException.validateNotNull( provisionSpec, "Provision spec" );
+
+        LOGGER.debug( "Scanning [" + provisionSpec.getPath() + "]" );
+
+        if( m_exception != null )
+        {
+            throw new ScannerException( "Exception while configuration", m_exception );
+        }
+
+        List<BundleReference> references = new ArrayList<BundleReference>();
+
+        final Integer defaultStartLevel = getDefaultStartLevel( provisionSpec, m_scannerConfiguration );
+        final Boolean defaultStart = getDefaultStart( provisionSpec, m_scannerConfiguration );
+        final Boolean defaultUpdate = getDefaultUpdate( provisionSpec, m_scannerConfiguration );
+
+        try
+        {
+            List<ArtifactBasicMetadata> query = new ArrayList<ArtifactBasicMetadata>();
+            final ArtifactMetadata queryMeta = new ArtifactMetadata( provisionSpec.getPath() );
+            query.add( queryMeta );
+
+            final ArtifactBasicResults results = m_vrr.readVersions( query );
+            if( results.hasExceptions() )
+            {
+                throw new ScannerException( results.getError( queryMeta ).getMessage() );
+            }
+            if( results.hasResults( queryMeta ) )
+            {
+                final List<ArtifactBasicMetadata> foundArtifacts = results.getResult( queryMeta );
+                for( ArtifactBasicMetadata foundArtifact : foundArtifacts )
+                {
+                    final ArtifactResults artifactResults = m_vrr.readArtifacts( toList( foundArtifact ) );
+                    if( artifactResults.hasExceptions() )
+                    {
+                        throw new ScannerException( artifactResults.getError( foundArtifact ).getMessage() );
+                    }
+                    if( artifactResults.hasResults( foundArtifact ) )
+                    {
+                        final List<Artifact> artifacts = artifactResults.getResults( foundArtifact );
+                        for( Artifact artifact : artifacts )
+                        {
+                            references.add(
+                                new FileBundleReference(
+                                    artifact.getFile().toURL().toExternalForm(),
+                                    defaultStartLevel, defaultStart, defaultUpdate
+                                )
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        catch( MalformedURLException e )
+        {
+            throw new ScannerException( e.getMessage() );
+        }
+        catch( RepositoryException e )
+        {
+            throw new ScannerException( e.getMessage() );
+        }
+
+        return references;
+    }
+
+    /**
+     * Initialize configurations and virtual repository reader.
+     */
+    private void initialize()
+        throws Exception
+    {
+        m_scannerConfiguration = createScannerConfiguration();
+        final MavenConfiguration mavenConfiguration = createMavenConfiguration();
+
+        final List<Repository> repositories = new ArrayList<Repository>();
+        final MavenRepositoryURL localRepository = mavenConfiguration.getLocalRepository();
+        LOGGER.debug( "Using local repository " + localRepository );
+        if( localRepository != null )
+        {
+            repositories.add(
+                new LocalRepositoryM2(
+                    "localRepo",
+                    new File( localRepository.toURL().toURI() ),
+                    DependencyProcessor.NULL_PROCESSOR
+                )
+            );
+        }
+        for( MavenRepositoryURL repositoryURL : mavenConfiguration.getRepositories() )
+        {
+            LOGGER.debug( "Using remote repository " + repositoryURL );
+            final Server server = new Server( repositoryURL.toURL().toExternalForm(), repositoryURL.toURL() );
+            final RemoteRepositoryM2 remoteRepo =
+                new RemoteRepositoryM2( server.getId(), server, DependencyProcessor.NULL_PROCESSOR );
+            remoteRepo.setRepositoryQualityRange(
+                createQualityRange( repositoryURL.isReleasesEnabled(), repositoryURL.isSnapshotsEnabled() )
+            );
+            repositories.add( remoteRepo );
+        }
+        m_vrr = new VirtualRepositoryReader( repositories );
+    }
+
+    /**
+     * TODO remove once a version > 1.0-alpha-5 of Mercury
+     */
+    private static QualityRange createQualityRange( boolean releases, boolean snapshots )
+    {
+        if( releases && snapshots )
+        {
+            return QualityRange.ALL;
+        }
+        else if( releases )
+        {
+            return new QualityRange( Quality.ALPHA_QUALITY, true, Quality.RELEASE_QUALITY, true );
+        }
+        else if( snapshots )
+        {
+            return new QualityRange( Quality.SNAPSHOT_QUALITY, true, Quality.SNAPSHOT_TS_QUALITY, true );
+        }
+
+        throw new IllegalArgumentException( "Unsuported combination for releases/snapshots" );
+    }
+
+    /**
+     * Utility to add var args elements into a list.
+     *
+     * @param elements to add
+     * @param <T>      any type
+     *
+     * @return list
+     */
+    private <T> List<T> toList( final T... elements )
+    {
+        final ArrayList<T> list = new ArrayList<T>();
+        list.addAll( Arrays.asList( elements ) );
+        return list;
+    }
+
+    /**
+     * Returns the default start level by first looking at the parser and if not set fallback to configuration.
+     *
+     * @param provisionSpec a parser
+     * @param config        a configuration
+     *
+     * @return default start level or null if nos set.
+     */
+    private Integer getDefaultStartLevel( ProvisionSpec provisionSpec, ScannerConfiguration config )
+    {
+        Integer startLevel = provisionSpec.getStartLevel();
+        if( startLevel == null )
+        {
+            startLevel = config.getStartLevel();
+        }
+        return startLevel;
+    }
+
+    /**
+     * Returns the default start by first looking at the parser and if not set fallback to configuration.
+     *
+     * @param provisionSpec a parser
+     * @param config        a configuration
+     *
+     * @return default start level or null if nos set.
+     */
+    private Boolean getDefaultStart( final ProvisionSpec provisionSpec, final ScannerConfiguration config )
+    {
+        Boolean start = provisionSpec.shouldStart();
+        if( start == null )
+        {
+            start = config.shouldStart();
+        }
+        return start;
+    }
+
+    /**
+     * Returns the default update by first looking at the parser and if not set fallback to configuration.
+     *
+     * @param provisionSpec a parser
+     * @param config        a configuration
+     *
+     * @return default update or null if nos set.
+     */
+    private Boolean getDefaultUpdate( final ProvisionSpec provisionSpec, final ScannerConfiguration config )
+    {
+        Boolean update = provisionSpec.shouldUpdate();
+        if( update == null )
+        {
+            update = config.shouldUpdate();
+        }
+        return update;
+    }
+
+    /**
+     * Sets the propertyResolver to use.
+     *
+     * @param propertyResolver a propertyResolver
+     */
+    public void setResolver( final PropertyResolver propertyResolver )
+    {
+        NullArgumentException.validateNotNull( propertyResolver, "PropertyResolver" );
+        m_propertyResolver = propertyResolver;
+        try
+        {
+            initialize();
+            m_exception = null;
+        }
+        catch( Exception e )
+        {
+            m_exception = e;
+        }
+    }
+
+    /**
+     * Creates a new scanner configuration.
+     *
+     * @return a configuration
+     */
+    ScannerConfiguration createScannerConfiguration()
+    {
+        return new ScannerConfigurationImpl( m_propertyResolver, ServiceConstants.PID );
+    }
+
+    /**
+     * Creates a new scanner configuration.
+     *
+     * @return a configuration
+     */
+    MavenConfiguration createMavenConfiguration()
+    {
+        final MavenConfigurationImpl configuration =
+            new MavenConfigurationImpl( m_propertyResolver, ServiceConstants.PID );
+        configuration.setSettings( new MavenSettingsImpl( configuration.getSettingsFileUrl() ) );
+        return configuration;
+    }
+
+}
