d0494f2c97291b50fc5ef981feb9fd5b1f67dbe4
tonit <tonit@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-04-27 17:01:16 +0000

    PAXRUNNER-177 use workingFolder parameter
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@15091 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner-platform/src/main/java/org/ops4j/pax/runner/platform/ScriptJavaRunner.java
+++ pax-runner-platform/src/main/java/org/ops4j/pax/runner/platform/ScriptJavaRunner.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2008 Alin Dreghiciu.
  * Copyright 2009 Mike Smoot.
+ * Copyright 2009 Toni Menzel.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,19 +25,17 @@ import java.io.IOException;
 import java.util.Arrays;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.ops4j.io.Pipe;
-import org.ops4j.pax.runner.commons.Info;
 import org.ops4j.pax.runner.platform.internal.CommandLineBuilder;
 
 /**
- * A Java Runner that writes shell and batch scripts in the 
+ * A Java Runner that writes shell and batch scripts in the
  * runner directory that start the specified OSGi framework.
  *
  * @author Alin Dreghiciu (adreghiciu@gmail.com)
  * @author Mike Smoot (msmoot@ucsd.edu)
  */
 public class ScriptJavaRunner
-	implements StoppableJavaRunner
+    implements StoppableJavaRunner
 {
 
     /**
@@ -83,20 +82,20 @@ public class ScriptJavaRunner
 
         LOG.debug( "Start command line [" + Arrays.toString( commandLine.toArray() ) + "]" );
 
-		final String shell = getShellScript(commandLine);
-		final String batch = getBatchScript(commandLine);
+        final String shell = getShellScript( commandLine );
+        final String batch = getBatchScript( commandLine );
 
         try
         {
             LOG.debug( "Writing run scripts." );
 
-			FileWriter sh = new FileWriter("runner/run.sh");
-			sh.write(shell);
-			sh.close();
+            FileWriter sh = new FileWriter( new File( workingDirectory, "run.sh" ) );
+            sh.write( shell );
+            sh.close();
 
-			FileWriter bat = new FileWriter("runner/run.bat");
-			bat.write(batch);
-			bat.close();
+            FileWriter bat = new FileWriter( new File( workingDirectory, "run.bat" ) );
+            bat.write( batch );
+            bat.close();
         }
         catch( IOException e )
         {
@@ -104,36 +103,38 @@ public class ScriptJavaRunner
         }
     }
 
-	public void shutdown() {}
+    public void shutdown()
+    {
+    }
 
-	private String getShellScript(final CommandLineBuilder commandLine) 
-	{
-		final String newline = "\n"; 
-		final StringBuilder script = new StringBuilder();
-		script.append("#!/bin/sh");
-		script.append(newline);
-		for ( String s : commandLine.toArray() ) 
-		{
-			script.append(s);
-			script.append(" ");
-		}
-		script.append("\"$@\"");
-		script.append(newline);
-		return script.toString();
-	}
+    private String getShellScript( final CommandLineBuilder commandLine )
+    {
+        final String newline = "\n";
+        final StringBuilder script = new StringBuilder();
+        script.append( "#!/bin/sh" );
+        script.append( newline );
+        for( String s : commandLine.toArray() )
+        {
+            script.append( s );
+            script.append( " " );
+        }
+        script.append( "\"$@\"" );
+        script.append( newline );
+        return script.toString();
+    }
 
-	private String getBatchScript(final CommandLineBuilder commandLine) 
-	{
-		final String newline = "\r\n";
-		final StringBuilder script = new StringBuilder();
-		script.append(newline);
-		for ( String s : commandLine.toArray() ) 
-		{
-			script.append(s);
-			script.append(" ");
-		}
-		script.append("%*");
-		script.append(newline);
-		return script.toString();
-	}
+    private String getBatchScript( final CommandLineBuilder commandLine )
+    {
+        final String newline = "\r\n";
+        final StringBuilder script = new StringBuilder();
+        script.append( newline );
+        for( String s : commandLine.toArray() )
+        {
+            script.append( s );
+            script.append( " " );
+        }
+        script.append( "%*" );
+        script.append( newline );
+        return script.toString();
+    }
 }
