62315bb54c29ff109d06e0a032d99baef501522a
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-05-22 13:17:51 +0000

    Upgrade scripts to next version
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@15260 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner/pax-run.sh
+++ pax-runner/pax-run.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.20.0-SNAPSHOT.jar "$@"
+java $JAVA_OPTS -jar $SCRIPTS/target/pax-runner-0.20.1-SNAPSHOT.jar "$@"
