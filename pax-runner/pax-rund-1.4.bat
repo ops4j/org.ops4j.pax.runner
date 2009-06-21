db1ffae0f7a7f9cea7542406cf00c131de37ad20
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-06-21 22:16:19 +0000

    Next version will be 1.0.0
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@15513 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner/pax-rund-1.4.bat
+++ pax-runner/pax-rund-1.4.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-0.21.0-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.0.0-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %1 %2 %3 %4 %5 %6 %7 %8 %9
--- pax-runner/pax-rund.sh
+++ pax-runner/pax-rund.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-exec java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-0.21.0-SNAPSHOT.jar org.ops4j.pax.runner.daemon.DaemonLauncher "$@"
+exec java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-1.0.0-SNAPSHOT.jar org.ops4j.pax.runner.daemon.DaemonLauncher "$@"
