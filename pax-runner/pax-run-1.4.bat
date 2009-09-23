712f7d9158c9223c380a46b4e864034994cb02f0
adreghiciu@gmail.com <adreghiciu@gmail.com@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2009-09-23 08:22:34 +0000

    Prepare for next release: 1.2.1
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@16301 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner/pax-run-1.4.bat
+++ pax-runner/pax-run-1.4.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.2.2-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.Run %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.2.1-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.Run %1 %2 %3 %4 %5 %6 %7 %8 %9
