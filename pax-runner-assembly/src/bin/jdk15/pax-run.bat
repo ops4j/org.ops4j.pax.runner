248bf3c0ae55a2efc61c6a7728144e9e9b1dbceb
xfournet <xfournet@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2010-09-10 16:14:18 +0000

    [PAXRUNNER-47] update bat files to avoid using quotes around options
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/runner@17015 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-runner-assembly/src/bin/jdk15/pax-run.bat
+++ pax-runner-assembly/src/bin/jdk15/pax-run.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-${project.version}.jar" org.ops4j.pax.runner.Run %1 %2 %3 %4 %5 %6 %7 %8 %9 
+java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-${project.version}.jar" org.ops4j.pax.runner.Run %*
