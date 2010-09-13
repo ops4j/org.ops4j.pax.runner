e1fea3f3a7487cde9e131d685cc290202cd5b7a0
tonit <toni@okidokiteam.com>
2010-09-13 14:55:11 +0200

    fixed version reference after version bump

--- pax-runner/pax-rund-1.4.bat
+++ pax-runner/pax-rund-1.4.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.5.0-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
+java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.0-SNAPSHOT-jdk14.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
