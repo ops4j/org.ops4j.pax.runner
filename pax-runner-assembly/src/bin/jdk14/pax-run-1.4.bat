e0baaceafce6cc30a345e60b8743766956dab87b
Andreas Pieber <anpieber@gmail.com>
2011-03-01 10:24:15 +0100

    [PAXRUNNER-371] drop jdk14 support
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- pax-runner-assembly/src/bin/jdk14/pax-run-1.4.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SETLOCAL
-set _SCRIPTS_=%~dp0
-
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-${project.version}-jdk14.jar" org.ops4j.pax.runner.Run %*
--- pax-runner-assembly/src/bin/jdk14/pax-rund-1.4.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SETLOCAL
-set _SCRIPTS_=%~dp0
-
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\pax-runner-${project.version}-jdk14.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
