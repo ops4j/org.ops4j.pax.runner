d653d473a902e87181f1423dfa06aa2bfc848e2c
tonit <toni@okidokiteam.com>
2011-03-02 16:53:28 +0100

    set version in shell scripts to 1.7.0-SNAPSHOT

--- pax-runner/pax-rund.bat
+++ pax-runner/pax-rund.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.0-SNAPSHOT.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
+java -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.7.0-SNAPSHOT.jar" org.ops4j.pax.runner.daemon.DaemonLauncher %*
