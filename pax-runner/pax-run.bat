40ffbaae54ebd2f7fc0493ad1c4e4d45e4a1703b
Andreas Pieber <anpieber@gmail.com>
2011-03-03 22:40:55 +0100

    Upgrade script version to 1.6.2-SNAPSHOT
    This reverts commit d12e3cb9d1930370fc2615902de69aca81689313.
    
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- pax-runner/pax-run.bat
+++ pax-runner/pax-run.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.1.jar" org.ops4j.pax.runner.Run %*
+java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.2-SNAPSHOT.jar" org.ops4j.pax.runner.Run %*
