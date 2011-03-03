5fecc04ecbbbb9313436f9daa4103281d2980b82
Andreas Pieber <anpieber@gmail.com>
2011-03-03 11:33:14 +0100

    Also update script version to 1.6.1-SNAPSHOT
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- pax-runner/pax-run.bat
+++ pax-runner/pax-run.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.0-SNAPSHOT.jar" org.ops4j.pax.runner.Run %*
+java %JAVA_OPTS% -cp ".;%_SCRIPTS_%;%_SCRIPTS_%\target\pax-runner-1.6.1-SNAPSHOT.jar" org.ops4j.pax.runner.Run %*
