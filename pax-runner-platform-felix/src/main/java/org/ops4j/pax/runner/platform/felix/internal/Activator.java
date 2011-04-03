b3e64cdc38eade5555de42b4ce1894f35d6dc712
Dmytro Pishchukhin <dmytro.pishchukhin@gmail.com>
2011-04-03 12:39:06 +0200

    [PAXRUNNER-384] Support Felix 3.2.0

--- pax-runner-platform-felix/src/main/java/org/ops4j/pax/runner/platform/felix/internal/Activator.java
+++ pax-runner-platform-felix/src/main/java/org/ops4j/pax/runner/platform/felix/internal/Activator.java
@@ -68,6 +68,7 @@ public final class Activator
             new FelixPlatformBuilderF160( bundleContext, "3.0.7" ),
             new FelixPlatformBuilderF160( bundleContext, "3.0.8" ),
             new FelixPlatformBuilderF160( bundleContext, "3.0.9" ),
+            new FelixPlatformBuilderF160( bundleContext, "3.2.0" ),
             new FelixPlatformBuilderSnapshot( bundleContext )
         };
     }
