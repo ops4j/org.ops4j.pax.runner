92e14b50a8ffff33230bdd336a7f6683ff688a1d
Toni Menzel <toni@okidokiteam.com>
2011-05-20 08:48:12 +0200

    use latest snapshot in script

--- pax-runner/pax-run.sh
+++ pax-runner/pax-run.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-1.7.1.jar org.ops4j.pax.runner.Run "$@"
+java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-1.7.2-SNAPSHOT.jar org.ops4j.pax.runner.Run "$@"
