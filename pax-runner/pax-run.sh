e13ff275e767d6dd2ce01e4ac7bb790317a4173d
Toni Menzel <toni@okidokiteam.com>
2011-05-28 15:57:46 +0200

    upgrade pax run script

--- pax-runner/pax-run.sh
+++ pax-runner/pax-run.sh
@@ -12,4 +12,4 @@ else
   SCRIPTS=`dirname $0`
 fi
 
-java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-1.7.3-SNAPSHOT.jar org.ops4j.pax.runner.Run "$@"
+java $JAVA_OPTS -cp .:$SCRIPTS:$SCRIPTS/target/pax-runner-1.7.4-SNAPSHOT.jar org.ops4j.pax.runner.Run "$@"
