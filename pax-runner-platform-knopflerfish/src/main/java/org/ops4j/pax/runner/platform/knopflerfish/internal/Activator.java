968244fe1309a63577dfa14b7b25bfc1e4ea74db
Dmytro Pishchukhin <dmytro.pishchukhin@gmail.com>
2012-12-01 14:38:21 +0100

    [PAXRUNNER-429] - KF 3.3.0 and 3.4.0 added

--- pax-runner-platform-knopflerfish/src/main/java/org/ops4j/pax/runner/platform/knopflerfish/internal/Activator.java
+++ pax-runner-platform-knopflerfish/src/main/java/org/ops4j/pax/runner/platform/knopflerfish/internal/Activator.java
@@ -55,6 +55,8 @@ public final class Activator
             new KnopflerfishPlatformBuilderF300( bundleContext, "3.0.0" ),
             new KnopflerfishPlatformBuilderF300( bundleContext, "3.1.0" ),
             new KnopflerfishPlatformBuilderF300( bundleContext, "3.2.0" ),
+            new KnopflerfishPlatformBuilderF300( bundleContext, "3.3.0" ),
+            new KnopflerfishPlatformBuilderF300( bundleContext, "3.4.0" ),
             new KnopflerfishPlatformBuilderSnapshot( bundleContext )
         };
     }
